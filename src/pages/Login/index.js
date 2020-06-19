aa42a622f09ddacc0618b2b38da0eb3d71e9efc0
mir4cles <jurimiralles@gmail.com>
2020-06-19 15:05:49 +0200

    setup events.map with Cards

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -12,7 +12,6 @@ import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -55,50 +54,12 @@ export default function SignUp() {
   function submitForm(event) {
     console.log("hi");
     event.preventDefault();
-
     dispatch(login(email, password));
-
     setEmail("");
     setPassword("");
   }
 
   return (
-    // <Container>
-    //   <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
-    //     <h1 className="mt-5 mb-5">Login</h1>
-    //     <Form.Group controlId="formBasicEmail">
-    //       <Form.Label>Email address</Form.Label>
-    //       <Form.Control
-    //         value={email}
-    //         onChange={event => setEmail(event.target.value)}
-    //         type="email"
-    //         placeholder="Enter email"
-    //         required
-    //       />
-    //     </Form.Group>
-
-    //     <Form.Group controlId="formBasicPassword">
-    //       <Form.Label>Password</Form.Label>
-    //       <Form.Control
-    //         value={password}
-    //         onChange={event => setPassword(event.target.value)}
-    //         type="password"
-    //         placeholder="Password"
-    //         required
-    //       />
-    //     </Form.Group>
-    //     <Form.Group className="mt-5">
-    //       <Button variant="primary" type="submit" onClick={submitForm}>
-    //         Log in
-    //       </Button>
-    //     </Form.Group>
-    //     <Link to="/signup" style={{ textAlign: "center" }}>
-    //       Click here to sign up
-    //     </Link>
-    //   </Form>
-    // </Container>
-    ////////////////////////////////
-    ////////////////////////////////
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
