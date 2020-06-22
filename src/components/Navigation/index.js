0e78ffba98c421e74ddb972e3aee04dec9efc387
mir4cles <jurimiralles@gmail.com>
2020-06-22 15:50:06 +0200

    removed button from import

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,7 +5,6 @@ import { Link as RouterLink } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Link from "@material-ui/core/Link";
