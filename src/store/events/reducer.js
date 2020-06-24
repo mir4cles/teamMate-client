fb86e69826193dbadd3e79429d1d6022e5bcca8b
mir4cles <jurimiralles@gmail.com>
2020-06-24 21:13:43 +0200

    removed update_events from reducer

--- src/store/events/reducer.js
+++ src/store/events/reducer.js
@@ -1,5 +1,4 @@
 import { FETCH_EVENTS_SUCCESS } from "./actions";
-import { UPDATE_EVENTS } from "../user/actions";
 
 const initialState = [];
 
@@ -7,19 +6,6 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_EVENTS_SUCCESS:
       return [...state, ...action.payload];
-
-    case UPDATE_EVENTS:
-      return {
-        ...state,
-        ...action.payload,
-      };
-
-    // case HEART_INCREMENT:
-    //   return state.map((artwork) =>
-    //     artwork.id === action.payload
-    //       ? { ...artwork, hearts: artwork.hearts + 1 }
-    //       : artwork
-    //   );
 
     default:
       return state;
